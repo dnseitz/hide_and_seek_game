781018cdff4f1768668c8a1f21c39fdc4cea9644
dnseitz <dnseitz@gmail.com>
2026-07-15 07:06:26 -0700

    refactor monster vision shader to allow control from scripts

--- characters/shared/player_controller.gd
+++ characters/shared/player_controller.gd
@@ -1,41 +1,19 @@
-class_name PlayerController extends Node3D
+class_name PlayerController extends CharacterBody3D
 
+const SPEED = 5.0
+const JUMP_VELOCITY = 4.5
+
+@export_group("Mouse")
 # TODO: Pull this from settings menu
 @export var mouse_sensitivity: float = 0.25
 # TODO: Radian slider in editor
 @export var tilt_upper_limit := PI / 3.0
 @export var tilt_lower_limit := -PI / 6.0
 
-# @export 
-
-@onready var _character_body: CharacterBody3D = $CharacterBody3D
 @onready var _camera_pivot: Node3D = $CameraPivot
 @onready var _camera: Camera3D = $CameraPivot/Camera3D
 
 var _cam_input_direction := Vector2.ZERO
-
-# func _process(delta: float) -> void:
-# 	_handle_movement(delta)
-
-func _physics_process(delta: float) -> void:
-	_camera_pivot.rotation.x += _cam_input_direction.y * delta
-	_camera_pivot.rotation.x = clamp(_camera_pivot.rotation.x, tilt_lower_limit, tilt_upper_limit)
-	_camera_pivot.rotation.y -= _cam_input_direction.x * delta
-
-	_cam_input_direction = Vector2.ZERO
-
-	var raw_direction := _get_raw_movement_direction()
-	var forward := _camera.global_basis.z
-	var right := _camera.global_basis.x
-
-	var direction = forward * raw_direction.z + right * raw_direction.x
-	if direction.length() > 0.0:
-		direction = direction.normalized()
-
-	_character_body.velocity = direction * 5.0
-	_character_body.move_and_slide()
-	print("CHARACTER VELOCITY: ", _character_body.velocity)
-	global_position = _character_body.global_position
 
 func _input(event: InputEvent) -> void:
 	if event.is_action_released("debug_left_click"):
@@ -54,13 +32,30 @@ func _unhandled_input(event: InputEvent) -> void:
 	
 	_cam_input_direction = mouse_motion_event.screen_relative * mouse_sensitivity
 
-func _get_raw_movement_direction() -> Vector3:
-	# var target_velocity := Vector3.ZERO
+func _physics_process(delta: float) -> void:
+	_camera_pivot.rotation.x += _cam_input_direction.y * delta
+	_camera_pivot.rotation.x = clamp(_camera_pivot.rotation.x, tilt_lower_limit, tilt_upper_limit)
+	rotation.y -= _cam_input_direction.x * delta
+	# _camera_pivot.rotation.y -= _cam_input_direction.x * delta
 
-	var raw_input := Input.get_vector("move_left", "move_right", "move_forward", "move_back")
+	_cam_input_direction = Vector2.ZERO
 
-	return Vector3(
-		raw_input.x,
-		0.0,
-		raw_input.y
-	)
+	# Add the gravity.
+	if not is_on_floor():
+		velocity += get_gravity() * delta
+
+	# Handle jump.
+	# if Input.is_action_just_pressed("ui_accept") and is_on_floor():
+	# 	velocity.y = JUMP_VELOCITY
+
+	# Get the input direction and handle the movement/deceleration.
+	var input_dir := Input.get_vector("move_right", "move_left", "move_back", "move_forward")
+	var direction := (transform.basis * Vector3(input_dir.x, 0, input_dir.y)).normalized()
+	if direction:
+		velocity.x = direction.x * SPEED
+		velocity.z = direction.z * SPEED
+	else:
+		velocity.x = move_toward(velocity.x, 0, SPEED)
+		velocity.z = move_toward(velocity.z, 0, SPEED)
+
+	move_and_slide()
