f7ac6c7d774c6aa7b6f61cabf0cc2b7d3dbbf6e8
dnseitz <dnseitz@gmail.com>
2026-07-23 07:00:35 -0700

    network optimizations

--- environment/props/_shared/prop.gd
+++ environment/props/_shared/prop.gd
@@ -5,9 +5,14 @@ const GROUP_NAME := &"environment_object"
 var _meshes: Array[MeshInstance3D] = []
 var _monster_vision_emitter: MonsterEnvironmentVisionEmitter
 
+@onready var _multiplayer_synchronizer: MultiplayerSynchronizer = %MultiplayerSynchronizer
+
 func _ready() -> void:
 	if multiplayer.is_server() == false:
+		# _multiplayer_synchronizer.process_mode = Node.PROCESS_MODE_DISABLED
+		collision_layer = 0 # Clear collision layer, all collisions are handled on the server
 		freeze = true
+
 	_meshes = []
 	for child in get_children():
 		if child is MeshInstance3D:
@@ -18,3 +23,28 @@ func _ready() -> void:
 			if _monster_vision_emitter != null:
 				push_warning("Multiple monster vision emitters found")
 			_monster_vision_emitter = child
+	
+	sleeping_state_changed.connect(_on_sleeping_state_changed)
+
+@rpc
+func _client_sleep(sleep: bool) -> void:
+	if sleep:
+		_multiplayer_synchronizer.process_mode = Node.PROCESS_MODE_DISABLED
+	else:
+		_multiplayer_synchronizer.process_mode = Node.PROCESS_MODE_INHERIT
+	
+#region callbacks
+# TODO: Add upper bound on number of synchronizers that can be unfrozen at 
+# a time? We'd have to play around with the number, ~30 seemed to start 
+# causing issues, that or batch the physics objects into a single 
+# synchronizer, maybe that improves performance?
+# Too many causes serious desync issues, I think it's taking too much time
+# processing in the tick loop and the rollback synchronization falls behind.
+func _on_sleeping_state_changed() -> void:
+	_client_sleep.rpc(sleeping)
+	_client_sleep(sleeping)
+	if sleeping:
+		_multiplayer_synchronizer.public_visibility = false
+	else:
+		_multiplayer_synchronizer.public_visibility = true
+#endregion
